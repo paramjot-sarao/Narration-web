17224a505d1e4990a26aaca3e3f1fbfa9ada6467
paramjot-sarao <param.sarao@gmail.com>
2016-04-23 13:40:35 +0530

    removed front

--- front/scales.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo $output->output;
-echo "<br>";
-echo "In the above table, the columns named Key1,Key2,....so on represents the scale values and";
-
-echo "<br>";
-echo "columns named Value1,Value2,....so on represents the interpretation of the values according to the different Likert scales.";
- ?>
